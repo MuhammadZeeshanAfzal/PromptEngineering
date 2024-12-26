3d3d67c84e5fb1b247807d46f7df04687c7eada9
Zeeshan <zeeshanafzal@pixelpk.com>
2024-12-26 15:54:19 +0500

    code updated

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def generate_article():
     return render_template("index.html", article=output)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
