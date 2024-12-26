19e8eb87b4418526bc48374d893bcac8cfaf2466
MuhammadZeeshanAfzal <zeeshanafzal1296@gmail.com>
2024-12-26 16:20:23 +0500

    Update app.py
    .

--- app.py
+++ app.py
@@ -102,5 +102,4 @@ def generate_article():
     return render_template("index.html", article=output)
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
