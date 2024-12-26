fe3fcedd6e591a1ec95e8a1c8ff85c072da303c2
MuhammadZeeshanAfzal <zeeshanafzal1296@gmail.com>
2024-12-26 16:15:13 +0500

    Update app.py
    it ok

--- app.py
+++ app.py
@@ -102,4 +102,5 @@ def generate_article():
     return render_template("index.html", article=output)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
